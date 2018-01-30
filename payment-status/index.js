d5e9b37f1be97046c6f16b26498698a3ed18f2ab
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2018-01-30 07:36:21 +0000

    Try with ws instead of socket.io

--- payment-status/index.js
+++ payment-status/index.js
@@ -1,49 +1,12 @@
-const {sample, random} = require('lodash');
-const server = require('http').createServer((request, response) => {
-  // Set CORS headers
-  console.log('request', request)
-  response.setHeader(
-    'Access-Control-Allow-Origin',
-    'http://neverssl.com'
-  );
-  response.setHeader('Access-Control-Request-Method', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-  response.setHeader(
-    'Access-Control-Allow-Headers',
-    'authorization, content-type'
-  );
-  if (request.method === 'OPTIONS') {
-    response.writeHead(200);
-    response.end();
-    return;
-  }
-});
+const WebSocket = require('ws');
 
-const io = require('socket.io')(server, {
-  origins: 'neverssl.com:80'
-});
+const wss = new WebSocket.Server({port: 9339});
 
-io.on('connection', client => {
-  console.log('someone connected');
-  client.on('event', data => {});
-  client.on('disconnect', () => {});
-});
+wss.on('connection', function connection(ws) {
+  console.log('connected')
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
 
-function updateStatus() {
-  io.emit(
-    'payment status',
-    `"${sample([
-      'processing',
-      'further processing',
-      'more processing',
-      'looking for the money',
-    ])}"`
-  );
-  setTimeout(updateStatus, random(1000, 10000));
-}
-updateStatus();
-
-const port = 9339;
-server.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
+  ws.send('something');
 });
